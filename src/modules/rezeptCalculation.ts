b5258bf707c3ab8bbf676e43a721b10f3034a4ee
永戸健太郎 <ngt@eitokentarounoMacBook-Air.local>
2023-02-12 17:27:26 +0900

    :sparkles: 計算関数追加

--- src/modules/rezeptCalculation.ts
+++ src/modules/rezeptCalculation.ts
@@ -1,15 +1,23 @@
 import { constants } from "../constants/const";
 class RezeptCalculation {
   percent: number;
-  originalPercent: number;
   percentString: string;
+  originalPercent: number;
+  originalTotal: number;
+  originalContact: number;
 
   constructor(private total: number, private contact: number) {
     this.percent = this.division(this.total, this.contact);
-    this.originalPercent = this.percent;
     this.percentString = this.parsePercent();
+    this.originalPercent = this.percent;
+    this.originalTotal = this.total;
+    this.originalContact = this.contact;
   }
-
+  /**
+   * method
+   * @param denominator 分母
+   * @param modules 分子
+   */
   division = (denominator: number, molecule: number): number => {
     if (denominator === 0) return 0;
     const num = (molecule / denominator) * 100;
@@ -35,6 +43,26 @@ class RezeptCalculation {
       this.division(this.total + 1, this.contact + 1) <=
       constants.TARGET_PERCENT
     );
+  };
+  calculation = () => {
+    if (this.checkPercentValForTop()) {
+      while (this.preCheckPercentValForTop()) {
+        this.total -= 1;
+        this.contact -= 1;
+        this.percent = this.division(this.total, this.contact);
+      }
+    } else {
+      while (this.preCheckPercentValForBottom()) {
+        this.total += 1;
+        this.contact += 1;
+        this.percent = this.division(this.total, this.contact);
+      }
+    }
+    return {
+      total: this.total,
+      contact: this.contact,
+      percent: this.parsePercent(),
+    };
   };
 }
 
--- tests/rezeptCaluculation.spec.ts
+++ tests/rezeptCaluculation.spec.ts
@@ -1,7 +1,7 @@
 import * as assert from "assert";
 import RezeptCalculation from "../src/modules/rezeptCalculation";
 
-describe("レセプトの計算クラスのテスト", (): void => {
+describe("レセプトの計算クラスの単体テスト", (): void => {
   test("初期値整数", (): void => {
     const rezept = new RezeptCalculation(100, 50);
     const percent = rezept.percent;
@@ -74,5 +74,36 @@ describe("レセプトの計算クラスのテスト", (): void => {
   test("pre加算パーセント確認アンダー", (): void => {
     const rezept = new RezeptCalculation(9999, 3983);
     expect(rezept.preCheckPercentValForBottom()).toBe(true);
+  });
+});
+
+describe("レセプトの計算クラスの結合テスト", (): void => {
+  test("計算せず", (): void => {
+    const rezept = new RezeptCalculation(10000, 3985);
+    const { total, contact, percent } = rezept.calculation();
+
+    assert.equal(total, 10000);
+    assert.equal(contact, 3985);
+    assert.equal(percent, "39.85%");
+  });
+  test("加算", (): void => {
+    const rezept = new RezeptCalculation(853, 337);
+    assert.equal(rezept.preCheckPercentValForTop(), false);
+    assert.equal(rezept.preCheckPercentValForBottom(), true);
+    const { total, contact, percent } = rezept.calculation();
+
+    assert.equal(total, 857);
+    assert.equal(contact, 341);
+    assert.equal(percent, "39.79%");
+  });
+  test("減算", (): void => {
+    const rezept = new RezeptCalculation(853, 500);
+    assert.equal(rezept.preCheckPercentValForTop(), true);
+    assert.equal(rezept.preCheckPercentValForBottom(), false);
+    const { total, contact, percent } = rezept.calculation();
+
+    assert.equal(total, 587);
+    assert.equal(contact, 234);
+    assert.equal(percent, "39.86%");
   });
 });
